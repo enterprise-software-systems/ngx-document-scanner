3cf36132eb2db3f643b226b58a855060a90b2888
balshawl <balshawl@essl.co.uk>
2023-05-15 13:11:41 +0100

    Auto-generated commit

--- projects/ngx-document-scanner/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-document-scanner
- */
-
-export * from './lib/ngx-document-scanner.module';
-export * from './lib/components/image-editor/ngx-doc-scanner.component';
-export * from './lib/PublicModels';
